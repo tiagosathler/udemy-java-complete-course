f7595f58d8959b368b7d93325563da9d2f6005be
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-02 22:36:58 -0300

    :sparkles: feat: exerícios da seção 21, conteúdo 268 - JavaConnector JDBC - acompanhamento da aula

---
+++ s21c268JavaConnector/src/db/DB.java
@@ -0,0 +1,46 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	private static Connection conn = null;
+
+	public static Connection getConnection() {
+		try {
+			if (conn == null) {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			}
+			return conn;
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+}
